357f84df10d972b47aaaa089423afd39aee32e56
Benjamen King <brk.3rdof3kings@gmail.com>
2018-02-03 23:20:50 -0700

    Implement all three prune-action buttons and effects

--- game.js
+++ game.js
@@ -5,7 +5,7 @@ game.drawTargetName(game.target);
 game.drawCounters(game.target);
 game.drawActionButtons(game.target);
 game.target.animateGrowth(game.target, 1, game.target.animationTimeDiff);
-game.activatePruneButton(game.target);
+game.activatePruneButtons(game.target);
 
 
 /* Target Class */
@@ -13,9 +13,9 @@ function Target(name, domID, totalSegments, maxSegments, maxPrunes, animationTim
     this.name = name;
     this.domID = domID; // id of the HTML element containing the target
     this.segmentCount = 0; // number of growth segments the target (tree) has added
-    this.currentSegmentNumber = 0; // the data-segOrder attribute of the last-added growth segment
+    this.currentSegmentNumber = 1; // the data-segOrder attribute of the last-added growth segment
     this.totalSegments = totalSegments;
-    this.maxSegments = maxSegments; // maximum number of segments allowed before game is lost (42 is minimum to win)
+    this.maxSegments = maxSegments; // maximum number of segments allowed before game is lost (31 is minimum to win)
     this.prunes = 0; // number of prune-actions enacted by user
     this.maxPrunes = maxPrunes; // maximum number of prune-actions allowed (11 is minimum to win)
     this.trunkSegments = trunkSegments;
@@ -36,23 +36,51 @@ function Target(name, domID, totalSegments, maxSegments, maxPrunes, animationTim
 
 /* Target Class methods */
 
-function prune() {
-    if (!this.trunkSegments.includes(this.currentSegmentNumber) && // No pruning the trunk!
-        this.currentSegmentNumber <= this.totalSegments) { // No pruning after the animation is over!
-
-        // Find the position where the trunk continues, following the current branch
-        const nextPostBranchSegmentNumber = this.postBranchSegments.find( branchPointNumber => {
-            return branchPointNumber >= this.currentSegmentNumber;
-        });
-        
-        // Halt current animation
-        this.timeouts.forEach( timeoutID => {
-            clearTimeout(timeoutID);
-        });
+function prune(multiplier) {
     
-        // Restart animation at next post-branch segment
-        this.updateCurrentSegment(nextPostBranchSegmentNumber);
-        this.animateGrowth(this, nextPostBranchSegmentNumber, this.animationTimeDiff);
+    if (this.prunes + multiplier <= this.maxPrunes) { // Can't exceed the maximum allowed number of prune actions
+        this.prunes += multiplier; // Update the prune count
+        const pruneCounterSpan = document.querySelector(`#${this.domID} .prunes-counter`);
+        pruneCounterSpan.innerText = this.prunes;
+    
+        if (!this.trunkSegments.includes(this.currentSegmentNumber) && // No pruning the trunk!
+            this.currentSegmentNumber <= this.totalSegments) { // No pruning after the animation is over!
+
+            const segment1Back = document.querySelector(`#${this.domID} .tree-seg[data-segOrder='${this.currentSegmentNumber - 1}']`);
+            const segment2Back = document.querySelector(`#${this.domID} .tree-seg[data-segOrder='${this.currentSegmentNumber - 2}']`);
+            const segment3Back = document.querySelector(`#${this.domID} .tree-seg[data-segOrder='${this.currentSegmentNumber - 3}']`);
+
+            if (multiplier >= 2) {
+                if (segment1Back && !this.trunkSegments.includes(segment1Back.getAttribute("data-segOrder"))) {
+                    segment1Back.classList.add('hidden');
+                    this.updateSegmentCount(-1);
+                }
+                if (segment2Back && !this.trunkSegments.includes(segment2Back.getAttribute("data-segOrder"))) {
+                    segment2Back.classList.add('hidden');
+                    this.updateSegmentCount(-1);
+                }
+            }
+            if (multiplier === 3) {
+                if (segment3Back && !this.trunkSegments.includes(segment3Back.getAttribute("data-segOrder"))) {
+                    segment3Back.classList.add('hidden');
+                    this.updateSegmentCount(-1);
+                }
+            }
+
+            // Find the position where the trunk continues after the current branch
+            const nextPostBranchSegmentNumber = this.postBranchSegments.find( branchPointNumber => {
+                return branchPointNumber >= this.currentSegmentNumber;
+            });
+            
+            // Halt the current animation
+            this.timeouts.forEach( timeoutID => {
+                clearTimeout(timeoutID);
+            });
+        
+            // Restart animation at the next trunk segment
+            this.updateCurrentSegment(nextPostBranchSegmentNumber);
+            this.animateGrowth(this, nextPostBranchSegmentNumber, this.animationTimeDiff);
+        }
     }
 }
 
@@ -68,7 +96,8 @@ function getSegments() {
 }
 
 /* Animate the target (tree) "growth" by unhiding its growth segments one at a time.
-   timeDifferential = number of miliseconds delay between unhiding sequential segments */
+   param timeDifferential = number of milliseconds delay between unhiding sequential segments.
+   param startingSegment = the segment number (i.e. data-segOrder attribute) at which to begin animation. */
 function animateGrowth(target, startingSegment, timeDifferential) {
 
     const segments = target.getSegments().slice(startingSegment - 1);
@@ -84,7 +113,7 @@ function animateGrowth(target, startingSegment, timeDifferential) {
         function unhide() {
             element.classList.toggle("hidden");
             target.updateSegmentCount(); // Also update the growth-segments count
-            target.updateCurrentSegment();
+            target.updateCurrentSegment(); // ...and the current-segment record
         }
         const timeoutID = setTimeout(unhide, interval);
         target.timeouts.push(timeoutID);
@@ -92,16 +121,20 @@ function animateGrowth(target, startingSegment, timeDifferential) {
 }
 
 function updateCurrentSegment(number) {
-    if (!number) {
+    if (!number) { // Update by 1 if no argument was given
         this.currentSegmentNumber += 1;
-    } else {
+    } else { // Otherwise, update to match the given argument
         this.currentSegmentNumber = number;
     }
 }
 
 // Update the count of growth segments that have been added to the target (tree)
-function updateSegmentCount() {
-    this.segmentCount += 1;
+function updateSegmentCount(number) {
+    if (!number) { // Update by 1 if no argument was given
+        this.segmentCount += 1;
+    } else { // Otherwise, update by the given argument
+        this.segmentCount += number;
+    }
     const segmentCounterSpan = document.querySelector(`#${this.domID} .segments-counter`);
     segmentCounterSpan.innerText = this.segmentCount;
 }
@@ -117,7 +150,7 @@ function addMods() {
 
 /* Game Class */
 function Game() {
-    this.target = new Target("blacktree", "black-tree", 144, 45, 20, 500, [
+    this.target = new Target("blacktree", "black-tree", 144, 54, 8, 800, [
         1,2,3,21,22,27,28,57,58,59,60,83,84,95,96,101,102,117,118,119,120,121,129,130,135,136,137,138,142,143,144
     ], [
         21, 27, 33, 57, 83, 95, 101, 117, 129, 135, 142
@@ -125,7 +158,7 @@ function Game() {
     this.drawTargetName = drawTargetName;
     this.drawCounters = drawCounters;
     this.drawActionButtons = drawActionButtons;
-    this.activatePruneButton = activatePruneButton;
+    this.activatePruneButtons = activatePruneButtons;
     
     // this.activate = activate;
     // this.drawStatus = drawStatus;
@@ -139,9 +172,22 @@ function Game() {
     // };
 }
 
-function activatePruneButton(target) {
-    const button = document.querySelector(`#${target.domID} .prune.btn`);
-    button.addEventListener("click", target.prune.bind(target));
+function activatePruneButtons(target) {
+    const buttons = document.querySelectorAll(`#${target.domID} .prune.btn`);
+    
+    buttons.forEach( button => {
+        button.addEventListener("click", () => {
+            let multiplier = 1;
+            const buttonClasses = Array.from(button.classList);
+            if (buttonClasses.includes("x2")) {
+                multiplier = 2;
+            }
+            if (buttonClasses.includes("x3")) {
+                multiplier = 3;
+            }
+            target.prune.bind(target)(multiplier);
+        });
+    });
 }
 
 function drawTargetName(target) {
@@ -178,9 +224,9 @@ function drawActionButtons(target) {
     targetDiv.appendChild(newDiv);
 
     const template = `
-        <button class="prune btn">PRUNE</button>
-        <button class="fertilize btn">FERTILIZE</button>
-        <button class="poison btn">POISON</button>
+        <button class="prune x1 btn">PRUNE (x1)</button>
+        <button class="prune x2 btn">PRUNE (x2)</button>
+        <button class="prune x3 btn">PRUNE (x3)</button>
     `;
     newDiv.innerHTML = template;
 }
