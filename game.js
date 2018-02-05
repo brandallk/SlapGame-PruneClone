7709f1dac3924c466750ee81f557742322440e4f
Benjamen King <brk.3rdof3kings@gmail.com>
2018-02-05 11:20:34 -0700

    Finish adding comments to game.js

--- game.js
+++ game.js
@@ -85,42 +85,53 @@ function prune(multiplier) {
         if (!this.trunkSegments.includes(this.currentSegmentNumber) && // No pruning the trunk!
             this.currentSegmentNumber <= this.totalSegments) { // No pruning after the animation is over!
 
+            // Find the position where the trunk continues after the current branch
+            const nextPostBranchSegmentNumber = this.postBranchSegments.find( branchPointNumber => {
+                return branchPointNumber >= this.currentSegmentNumber;
+            });
+            
+            // Get references to growth segments added immediately previous to the segment currently being "pruned"
             const segment1Back = document.querySelector(`#${this.domID} .tree-seg[data-segorder='${this.currentSegmentNumber - 1}']`);
             const segment2Back = document.querySelector(`#${this.domID} .tree-seg[data-segorder='${this.currentSegmentNumber - 2}']`);
             const segment3Back = document.querySelector(`#${this.domID} .tree-seg[data-segorder='${this.currentSegmentNumber - 3}']`);
 
             if (multiplier >= 2) {
+                // If the previous segment exists and does NOT belong to the main trunk, hide it and decrement the tree's segmentCount
                 if (segment1Back && !this.trunkSegments.includes(Number(segment1Back.getAttribute("data-segorder")))) {
                     segment1Back.classList.add('hidden');
                     this.updateSegmentCount(-1);
                 }
+                // If the previous - 1 segment exists and does NOT belong to the main trunk, hide it and decrement the tree's segmentCount
                 if (segment2Back && !this.trunkSegments.includes(Number(segment2Back.getAttribute("data-segorder")))) {
                     segment2Back.classList.add('hidden');
                     this.updateSegmentCount(-1);
                 }
             }
             if (multiplier === 3) {
+                // If the previous - 2 segment exists and does NOT belong to the main trunk, hide it and decrement the tree's segmentCount
                 if (segment3Back && !this.trunkSegments.includes(Number(segment3Back.getAttribute("data-segorder")))) {
                     segment3Back.classList.add('hidden');
                     this.updateSegmentCount(-1);
                 }
             }
 
-            // Find the position where the trunk continues after the current branch
-            const nextPostBranchSegmentNumber = this.postBranchSegments.find( branchPointNumber => {
-                return branchPointNumber >= this.currentSegmentNumber;
-            });
-            
-            // Halt the current animation
-            this.timeouts.forEach( timeoutID => {
-                clearTimeout(timeoutID);
-            });
-        
-            // Restart animation at the next trunk segment
-            this.updateCurrentSegment(nextPostBranchSegmentNumber);
-            this.animateGrowth(this, nextPostBranchSegmentNumber, this.animationTimeDiff);
+            // Restart target (tree) growth animation from the next main-trunk segment following the pruned branch
+            restartAnimation(this, nextPostBranchSegmentNumber);
         }
     }
+}
+
+// Restart target (tree) growth animation from a given growth-segment number
+function restartAnimation(target, segNumber) {
+
+    // Halt the current animation
+    target.timeouts.forEach( timeoutID => {
+        clearTimeout(timeoutID);
+    });
+
+    // Restart animation at the next trunk segment
+    target.updateCurrentSegment(segNumber);
+    target.animateGrowth(target, segNumber, target.animationTimeDiff);
 }
 
 /* Animate the target (tree) "growth" by unhiding its growth segments one at a time.
@@ -138,30 +149,36 @@ function animateGrowth(target, startingSegment, timeDifferential) {
 
     // Unhide growth segments on a staggered time-schedule
     function delayDisplay(element, interval) {
+        const timeoutID = setTimeout(unhide, interval); // Set a timeout-delay for "unhiding" (i.e. displaying) the given (hidden) growth segment
+        target.timeouts.push(timeoutID); // Add the timeoutID to an array so this reference can be used to cancel the timeout later, if needed
+
+        // Callback function for the setTimeout above
         function unhide() {
+            // only proceed if the animation has not yet "unhidden" the max-allowed segment number
             if (target.segmentCount < target.maxSegments) {
-                element.classList.toggle("hidden");
+                element.classList.toggle("hidden"); // apply a CSS class to "unhide" the segment
             }
+            // If either the max segmentCount has been exceeded OR the animation has reached the last available segment, end the game
             if (target.segmentCount >= target.maxSegments ||
                 target.currentSegmentNumber === target.totalSegments) {
                     game.end(game.getResult(target));
-            } else {
+            } else { // ...otherwise, update the game state
                 if (target.segmentCount < target.maxSegments) {
-                    target.updateSegmentCount(); // Also update the growth-segments count
+                    target.updateSegmentCount(); // Update the growth-segments count
                     target.updateCurrentSegment(); // ...and the current-segment record
                 }
             }
         }
-
-        const timeoutID = setTimeout(unhide, interval);
-        target.timeouts.push(timeoutID);
     }
 }
 
 // Sort the target (tree) growth segments in order from the tree's base to its top
 function getSegments() {
+    // Get references to elements inside the tree SVG that correspond to growth segments
     const targetElt = document.querySelector(`#${this.domID}`);
     const segments = Array.from(targetElt.querySelectorAll(".tree-seg"));
+
+    // Sort acsending according to the SVG-internal element's "data-segOrder" attribute, which is the order in which the segments should be "unhidden" during animation
     return segments.sort( (segmentA, segmentB) => {
         segOrderA = segmentA.dataset.segorder;
         segOrderB = segmentB.dataset.segorder;
@@ -172,12 +189,15 @@ function getSegments() {
 /* Update the count of growth segments that have been added to the target (tree).
    param "number" is optional. */
 function updateSegmentCount(number) {
+    const segmentCounterSpan = document.querySelector(`#${this.domID} .segments-counter`);
+
     if (!number) { // Update by 1 if no argument was given
         this.segmentCount += 1;
     } else { // Otherwise, update by the given argument
         this.segmentCount += number;
     }
-    const segmentCounterSpan = document.querySelector(`#${this.domID} .segments-counter`);
+    
+    // Update the game display
     segmentCounterSpan.innerText = this.segmentCount;
 }
 
@@ -196,6 +216,8 @@ function updateCurrentSegment(number) {
 function toggleVisibility(state) {
     const targetElt = document.querySelector(`#${this.domID}`);
     const segments = Array.from(targetElt.querySelectorAll(".tree-seg"));
+
+    // Hide or show each tree segment by applying or removing a CSS class
     segments.forEach( segment => {
         if (state === "hide") {
             segment.classList.add("hidden");
@@ -261,7 +283,7 @@ function Game() {
     };
     this.inProgress = false; // boolean that keeps track of whether or not a game is running or stopped
 
-    this.drawCounters = drawCounters; // add HTML elements that report changing segment-count and prune-action count during game-play
+    this.drawCounters = drawCounters; // add or update HTML elements that report changing segment-count and prune-action count during game-play
     this.drawButtons = drawButtons; // add HTML buttons that can apply modifiers (before game-play) or prune-actions (during game-play)
     this.activate = activate; // apply a set of event handlers for buttons
 
@@ -272,19 +294,10 @@ function Game() {
     // this.drawTargetName = drawTargetName;  // (In this game's context, it doesn't really make sense to display the target's name.)
 }
 
-// Add HTML elements that report changing segment-count and prune-action count during game-play
+// Add or update HTML elements that report changing segment-count and prune-action count during game-play
 function drawCounters(target) {
     const targetDiv = document.querySelector(`#${target.domID}`);
     let countersDiv = document.querySelector(`#${target.domID} div.target-counters`);
-
-    if (countersDiv) {
-        countersDiv.innerHTML = "";
-    } else {
-        countersDiv = document.createElement("div");
-        countersDiv.className = "target-counters";
-        targetDiv.insertAdjacentElement('afterbegin', countersDiv);
-    }
-
     const template = `
         <div class="row">
             <div class="game-stats-counter col-10">
@@ -297,6 +310,15 @@ function drawCounters(target) {
             </div>
         </div>
     `;
+
+    if (countersDiv) { // If the div already exists, wipe out its previous content
+        countersDiv.innerHTML = "";
+    } else { // ...otherwise create it fresh
+        countersDiv = document.createElement("div");
+        countersDiv.className = "target-counters";
+        targetDiv.insertAdjacentElement('afterbegin', countersDiv);
+    }
+
     countersDiv.innerHTML = template;
 }
 
@@ -316,47 +338,53 @@ function activate() {
 
 // Start target (tree) growth animation and game-play
 function start() {
-    const targetSVG = document.querySelector(`#${this.target.domID} svg`);
-    targetSVG.classList.toggle("faded");
+    const targetTree = document.querySelector(`#${this.target.domID} svg`);
+    const blossoms = document.querySelector("img.tree-blossoms");
 
+    // Hide the faded-out tree and blossoms
+    targetTree.classList.toggle("faded"); // Prep the tree to display in unfaded state
     this.target.toggleVisibility("hide");
+    blossoms.classList.remove("faded"); // Prep the blossoms to display next time in unfaded state
+    blossoms.classList.add("hidden");
+
+    // Start the tree-growth animation
     this.target.animateGrowth(this.target, 1, this.target.animationTimeDiff);
+
+    // Update the game state and display
     this.inProgress = true;
     this.target.updateCurrentSegment();
     this.drawCounters(this.target);
-
-    const blossoms = document.querySelector("img.tree-blossoms");
-    blossoms.classList.remove("faded");
-    blossoms.classList.add("hidden");
 }
 
 // Stop target (tree) growth animation and game-play. Report game result (win/loss) to the user.
 function end(gameResult) {
+    const msgHeading = document.querySelector(".main-control .game-result");
+    const beginButton = document.querySelector(".main-control button.begin");
+    const resetButton = document.querySelector(".main-control button.reset");
+    const blossoms = document.querySelector("img.tree-blossoms");
+
     // Halt the current animation
     this.target.timeouts.forEach( timeoutID => {
         clearTimeout(timeoutID);
     });
-    this.inProgress = false;
+    this.inProgress = false; // update game state
 
-    const msgHeading = document.querySelector(".main-control .game-result");
+    // Indicate to the user whether the game was won or lost
     if (gameResult === "success") {
         msgHeading.innerText = "SUCCESS!";
-
-        const blossoms = document.querySelector("img.tree-blossoms");
         blossoms.classList.remove("hidden");
     } else {
         msgHeading.innerText = "FAILURE!";
     }
 
-    const beginButton = document.querySelector(".main-control button.begin");
-    const resetButton = document.querySelector(".main-control button.reset");
+    // Hide the start button; show the reset button
     beginButton.classList.add("hidden");
     resetButton.classList.remove("hidden");
 }
 
-// Determine if the game was won or lost: Compare max-allowed growth segments to the number of segments added by game-end
+// Determine if the game was won or lost
 function getResult(target) {
-    if (target.segmentCount < target.maxSegments) {
+    if (target.segmentCount < target.maxSegments) { // Compare max-allowed growth segments to the number of segments added by game-end
         return "success";
     }
     return "failure";
@@ -364,6 +392,12 @@ function getResult(target) {
 
 // Reset all the things back to their original state so a new, fresh game can be played
 function reset(target) {
+    const tree = document.querySelector(`#${this.target.domID} svg`);
+    const blossoms = document.querySelector("img.tree-blossoms");
+    const msgHeading = document.querySelector(".main-control .game-result");
+    const gameWindow = document.querySelector("div.game-window");
+    const handicapIndicator = document.querySelector("div.handicap span.indicator");
+
     target.segmentCount = 0;
     target.currentSegmentNumber = 0;
     target.maxSegments = target.originalMaxSegments;
@@ -376,43 +410,40 @@ function reset(target) {
     });
     target.modifiers = [];
 
-    const targetSVG = document.querySelector(`#${this.target.domID} svg`);
-    targetSVG.classList.toggle("faded");
-
+    // Also update the game display
+    tree.classList.toggle("faded");
     target.toggleVisibility("show");
-    this.drawCounters(target);
-
-    const blossoms = document.querySelector("img.tree-blossoms");
     blossoms.classList.add("faded");
     blossoms.classList.remove("hidden");
-
-    const msgHeading = document.querySelector(".main-control .game-result");
     msgHeading.innerText = "";
-
-    const gameWindow = document.querySelector("div.game-window");
-    const handicapIndicator = document.querySelector("div.handicap span.indicator");
     gameWindow.className = "game-window col-12 col-md-9";
     handicapIndicator.innerHTML = "no";
+    this.drawCounters(target);
 }
 
 // Add HTML buttons that can apply prune-actions
 function drawActionButtons() {
     const targetDiv = document.querySelector(".main-control div.row");
     const newDiv = document.createElement("div");
-    newDiv.className = "action-buttons col-12 mb-4 mb-md-0 col-md-6 text-center";
-    targetDiv.insertAdjacentElement('afterbegin', newDiv);
-
     const template = `
         <button class="prune x1 btn btn-sm btn-outline-danger">PRUNE (x1)</button>
         <button class="prune x2 btn btn-sm btn-outline-danger">PRUNE (x2)</button>
         <button class="prune x3 btn btn-sm btn-outline-danger">PRUNE (x3)</button>
     `;
+
+    newDiv.className = "action-buttons col-12 mb-4 mb-md-0 col-md-6 text-center";
+    targetDiv.insertAdjacentElement('afterbegin', newDiv);
     newDiv.innerHTML = template;
 }
 
 // Add HTML buttons that can apply modifiers
 function drawModifierButtons() {
     const targetDiv = document.querySelector(".main-control");
+    const template = `
+        <button class="modifier handicap btn btn-sm btn-outline-danger">HANDICAP</button>
+        <button class="modifier clouds btn btn-sm btn-outline-danger">CLOUDS</button>
+        <button class="modifier moonlight btn btn-sm btn-outline-danger">MOONLIGHT</button>
+    `;
 
     rowDiv = document.createElement("div");
     rowDiv.className = "row";
@@ -421,12 +452,7 @@ function drawModifierButtons() {
     buttonsDiv = document.createElement("div");
     buttonsDiv.className = "modifier-buttons col-12 col-md-6 text-center";
     rowDiv.insertAdjacentElement('afterbegin', buttonsDiv);
-
-    const template = `
-        <button class="modifier handicap btn btn-sm btn-outline-danger">HANDICAP</button>
-        <button class="modifier clouds btn btn-sm btn-outline-danger">CLOUDS</button>
-        <button class="modifier moonlight btn btn-sm btn-outline-danger">MOONLIGHT</button>
-    `;
+    
     buttonsDiv.innerHTML = template;
 }
 
@@ -458,7 +484,7 @@ function activatePruneButtons(game) {
     
     buttons.forEach( button => {
         button.addEventListener("click", () => {
-            if (game.inProgress) {
+            if (game.inProgress) { // Only allow the prune buttons to be active during game-play
                 let multiplier = 1;
                 const buttonClasses = Array.from(button.classList);
                 if (buttonClasses.includes("x2")) {
@@ -479,7 +505,7 @@ function activateModifierButtons(game) {
     
     buttons.forEach( button => {
         button.addEventListener("click", () => {
-            if (!game.inProgress) {
+            if (!game.inProgress) { // Only allow the modifier buttons to be used before a game starts
                 const buttonClasses = Array.from(button.classList);
                 if (buttonClasses.includes("handicap")) {
                     game.target.giveItem(game.modifiers.handicap);
